297d4543804a4373e48942b3d833682975bceb6a
Fabio Salvalai <fabio@seesharp.ch>
2021-12-02 10:06:28 +0100

    [1] listen to DOM changes and add a link to vscode
    right now, the absolute part is hardcoded.

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,13 +1,60 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+chrome.extension.sendMessage({}, function (response) {
+	var readyStateCheckInterval = setInterval(function () {
+		if (document.readyState === "complete") {
+			document.addEventListener('DOMNodeInserted', nodeInsertedCallback);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+			clearInterval(readyStateCheckInterval);
 
-	}
+			// ----------------------------------------------------------
+			// This part of the script triggers when page is done loading
+			console.log("Hello. This message was sent from scripts/inject.js");
+			// ----------------------------------------------------------
+
+		}
 	}, 10);
 });
+
+function nodeInsertedCallback(event) {
+	document.removeEventListener('DOMNodeInserted', nodeInsertedCallback)
+	console.log("Listing all the breadcrumbs...");
+	// handle links
+	const fileBreadcrumbs = document.querySelectorAll('a.file-breadcrumbs-segment-highlighted');
+	for (let index = 0; index < fileBreadcrumbs.length; index++) {
+		const element = fileBreadcrumbs[index];
+		const filePath = `/${element.hash.slice(1, element.hash.indexOf('?')+1)}`;
+
+		const existingLink = document.querySelector(`[data-linkFor='${element.href}']`);
+		if (!existingLink) {
+			const openInVsCodeNode = document.createElement('a');
+			openInVsCodeNode.setAttribute('data-linkFor', element.href)
+			openInVsCodeNode.href = `vscode://file/E:/sources/portail/${filePath}`;
+			openInVsCodeNode.textContent = 'open in vscode';
+			openInVsCodeNode.style = 'margin-left: 10px;';
+			element.after(openInVsCodeNode);
+			console.log(filePath);
+		}
+
+	}
+	const spanFileBreadcrumbs = document.querySelectorAll('span.file-breadcrumbs-segment-highlighted');
+	for (let index = 0; index < spanFileBreadcrumbs.length; index++) {
+		const element = spanFileBreadcrumbs[index];
+		const baseUri = new URL(element.baseURI);
+		const filePath = `/${baseUri.hash.slice(1)}`;
+
+		const existingLink = document.querySelector(`[data-linkFor='${baseUri}']`);
+		if (!existingLink) {
+			const openInVsCodeNode = document.createElement('a');
+			openInVsCodeNode.setAttribute('data-linkFor', baseUri)
+			openInVsCodeNode.href = `vscode://file/E:/sources/portail/${filePath}`;
+			openInVsCodeNode.textContent = 'open in vscode';
+			openInVsCodeNode.style = 'margin-left: 10px;';
+			element.after(openInVsCodeNode);
+		}
+
+		console.log(filePath);
+
+	}
+	console.log("...Listing over.");
+	document.addEventListener('DOMNodeInserted', nodeInsertedCallback);
+
+};
